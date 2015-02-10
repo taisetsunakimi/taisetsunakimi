036eb993c1add21696dae95ed084f68282316127
guangchuan <guangchuan.h@gmail.com>
2015-02-10 13:55:07 +0900

    mailersを削除しました。

--- app/mailers/post_mailer.rb
+++
@@ -1,9 +0,0 @@
-class PostMailer < ActionMailer::Base
-  default from: "root@taisetsunakimi.net"
-  default to: "guangchuan.h@gmail.com"
-
-  def post_email(user, post)
-    
-    mail subject: "title"
-  end
-end
