cb7a4ba74200cc6f70f2571ee76828dffe535fd6
guangchuan <guangchuan.h@gmail.com>
2015-03-20 14:59:47 +0900

    ID追加をテンプレート編集画面から行えるように修正。他（誤字修正）

--- app/mailers/nursequestion_mailer.rb
+++ app/mailers/nursequestion_mailer.rb
@@ -3,7 +3,7 @@ class NursequestionMailer < ActionMailer::Base
             to: "guangchuan.h@gmail.com"
   def nursequestionmail(question)
     @question = question
-    title = "保健師・看護師から質問を受け付けました。"
+    title = "保健師・助産師から質問を受け付けました。"
     mail(subject: title, template_path: 'nursequestionmail_mailer', template_name: 'nursquestionmail.text.erb')
   end
 end
