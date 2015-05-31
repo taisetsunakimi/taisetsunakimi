0e149a3bce76fd519133c01a5fb592fb334f2c7f
n_niibe <nozomu.nibe@llbe.jp>
2015-05-31 15:55:06 +0900

    エラーメッセージの体裁崩れ対応と、日本語化対応

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,7 @@ module Mongo
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ja
+    config.i18n.fallbacks = {'ja' => 'en'}
     config.action_view.field_error_proc = proc { |html_tag, instance| "<div class='has-error'>#{html_tag}</div>".html_safe }
   end
 end
