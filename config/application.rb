f3658d1b89064aafd685643a38b523dc101ff949
n_niibe <nozomu.nibe@llbe.jp>
2015-05-31 15:24:04 +0900

    入力エラー時に、スタイルが崩れる不具合を対応

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,6 @@ module Mongo
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ja
+    config.action_view.field_error_proc = proc { |html_tag, instance| "<div class='has-error'>#{html_tag}</div>".html_safe }
   end
 end
